17a30a05dcf4d2617866e6c4a20e1837f1eaeab6
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-30 21:58:49 -0300

    Configurado ESLint.

--- rangelflix/src/components/FormField/index.js
+++ rangelflix/src/components/FormField/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FormField = ({ label, type, name, value, onChange }) => {
+const FormField = ({
+  label, type, name, value, onChange,
+}) => {
+  const fieldId = `id_${name}`;
   return (
     <div>
-      <label>
-        {label}: 
-        <input
-          type={type}
-          name={name}
-          value={value}
-          onChange={onChange}
-        />
+      <label htmlFor={fieldId}>
+        {label}
+        :
+        <input id={fieldId} type={type} name={name} value={value} onChange={onChange} />
       </label>
     </div>
   );
+};
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 export default FormField;
